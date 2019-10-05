76abaac26e6f7360241bc885a89029f83a1a3f98
nikaperepelkina <nikakoda@yandex.ru>
2019-10-05 22:56:00 +0700

    Segmented control

--- ExPay/DataCollectionViewCell.swift
+++ ExPay/DataCollectionViewCell.swift
@@ -11,4 +11,49 @@ import UIKit
 class DataCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var labelMoney: UILabel!
+    @IBOutlet weak var labelValue: UILabel!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    var money = "2500"
+    
+    lazy var moneyArr = ["\(money)", "\(money)", "\(money)", "\(money)", "\(money)"]
+    lazy var valueArr = ["THB", "DRG", "BTC", "ETC", "ABC"]
+    
+    
+    @IBAction func choiceSegment(_ sender: UISegmentedControl) {
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+           labelMoney.text = moneyArr[0]
+            labelValue.text = valueArr[0]
+        case 1:
+            labelMoney.text = moneyArr[1]
+            labelValue.text = valueArr[1]
+        case 2:
+           labelMoney.text = moneyArr[2]
+            labelValue.text = valueArr[2]
+        case 3:
+            labelMoney.text = moneyArr[3]
+            labelValue.text = valueArr[3]
+        case 4:
+            labelMoney.text = moneyArr[4]
+            labelValue.text = valueArr[4]
+        default:
+            print("Что-то пошло не так!")
+        }
+        
+    }
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
--- ExPay/MyAccountVC.swift
+++ ExPay/MyAccountVC.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class MyAccountVC: UIViewController {
-
+    
+///////// origin
+   
+    
+  //  @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+   
+    
     /////  или string
     var money = "2500"
     
-    lazy var moneyArr = ["\(money) THB", "\(money) DRG", "\(money) BTC", "\(money) ETC", "\(money) ABC"]
-    
+   lazy var moneyArr = ["\(money)", "\(money)", "\(money)", "\(money)", "\(money)"]
+    lazy var valueArr = ["THB", "DRG", "BTC", "ETC", "ABC"]
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -28,30 +35,46 @@ class MyAccountVC: UIViewController {
         
         
         let color1 = #colorLiteral(red: 0.06666666667, green: 0.2274509804, blue: 0.3725490196, alpha: 1).cgColor
-              let color2 = #colorLiteral(red: 0.02352941176, green: 0.1019607843, blue: 0.1725490196, alpha: 1).cgColor
-              
-              let gradientLayer = CAGradientLayer()
-              gradientLayer.frame = self.view.bounds
-              gradientLayer.colors = [color1, color2]
-              gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.0)
-              gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
-              self.view.layer.insertSublayer(gradientLayer, at: 0)
+        let color2 = #colorLiteral(red: 0.02352941176, green: 0.1019607843, blue: 0.1725490196, alpha: 1).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [color1, color2]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
         
         
         view.endEditing(false)
     }
+    
+    
+    
+
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
+
+// расширения для labelMoney - отображения валюты и баланса
 
 extension MyAccountVC: UICollectionViewDelegate, UICollectionViewDataSource {
     
@@ -62,6 +85,7 @@ extension MyAccountVC: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? DataCollectionViewCell
         cell?.labelMoney.text = moneyArr[indexPath.row]
+    cell?.labelValue.text = valueArr[indexPath.row]
         return cell!
     }
 }
